e5e762fa018a7fd7ab67b28c8cf343de1b038591
Alec <alec4nandes@gmail.com>
2023-06-27 19:20:55 -0700

    move views

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
     return {
         dir: {
-            input: "public/views",
+            input: "views",
             output: "public",
         },
     };
