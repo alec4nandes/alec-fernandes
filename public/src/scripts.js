60014b53c74ab581f2209e2db80756351943ec31
Alec <alec4nandes@gmail.com>
2024-04-17 20:26:34 -0700

    feature: add hamburger menu

--- public/src/scripts.js
+++ public/src/scripts.js
@@ -12,3 +12,17 @@ document.querySelectorAll(".date-posted").forEach(
             .replace("AM", "am")
             .replace("PM", "pm"))
 );
+
+const openMenuBtn = document.querySelector("#hamburger-menu-button"),
+    closeMenuBtn = document.querySelector("#close-menu"),
+    menu = document.querySelector("#hamburger-menu");
+
+openMenuBtn.onclick = () => {
+    openMenuBtn.style.display = "none";
+    menu.classList.remove("closed");
+};
+
+closeMenuBtn.onclick = () => {
+    openMenuBtn.style.display = "block";
+    menu.classList.add("closed");
+};
