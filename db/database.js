067989d3b59d2e7cb97954c9b229b8afb9532244
Alec Fernandes <alecfernandes@Alec-Fernandes-MacBook-Pro.local>
2024-08-31 17:53:12 -0700

    transfer to mac

--- db/database.js
+++ db/database.js
@@ -1,7 +1,6 @@
 const admin = require("firebase-admin");
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
-
 const { serviceAccount } = require("./firebase-config.js");
 
 const app = initializeApp({
