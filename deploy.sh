d5e6602db773ad9dd9fb9bef6d0433418983ce84
Alec Fernandes <alecfernandes@Alec-Fernandes-MacBook-Pro.local>
2024-09-24 23:18:29 -0700

    fix deploy replace

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-sed -i '' 's/DEV/BLOG/g' public/admin/scripts/openai.mjs
+sed -i '' 's/"DEV"/"BLOG"/g' public/admin/scripts/openai.mjs
 sed -i '' 's/db-dev.mjs/db-prod.mjs/g' public/admin/scripts/openai.mjs
 sed -i '' 's/db-dev.mjs/db-prod.mjs/g' public/admin/scripts/auth.mjs
 
@@ -6,4 +6,4 @@ npm run reset && firebase deploy && npm run deploy-tweet
 
 sed -i '' 's/db-prod.mjs/db-dev.mjs/g' public/admin/scripts/auth.mjs
 sed -i '' 's/db-prod.mjs/db-dev.mjs/g' public/admin/scripts/openai.mjs
-sed -i '' 's/BLOG/DEV/g' public/admin/scripts/openai.mjs
+sed -i '' 's/"BLOG"/"DEV"/g' public/admin/scripts/openai.mjs
